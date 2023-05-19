7a7fe9944fcb1f08c93a50f9f92df034389a006b
Fady Abdelmalik <fady@fadymak.com>
2023-05-19 20:13:43 +0200

    add page to subscribe to events

--- routes/events.ts
+++ routes/events.ts
@@ -6,20 +6,15 @@ export default {
   async handler() {
     let eventCount = 0;
 
-    // create a stream that emits events every 500ms for 25 times
+    // create a stream that emits events every 500ms to simulate doing work
+    // (e.g. polling a database for new data, fetching new data from an API, etc.)
     const stream = new ReadableStream({
       start(controller) {
         controller.enqueue("data: stream started\n\n");
       },
       async pull(controller) {
-        if (eventCount === 25) {
-          controller.close();
-        }
-
         controller.enqueue(`id: ${eventCount}\n`);
-        controller.enqueue(
-          `data: event at ${new Date().toLocaleTimeString()}\n\n`
-        );
+        controller.enqueue(`data: event #${eventCount}\n\n`);
 
         eventCount++;
         return new Promise((r) => setTimeout(r, 500));
